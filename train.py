9adbf3fab886b360362964ede0b97f4b0d33c8e7
Devendra61 <devbarahen@gmail.com>
2023-02-14 13:31:41 +0530

    acc change

--- train.py
+++ train.py
@@ -17,7 +17,7 @@ clf.fit(X_train, y_train)
 acc = clf.score(X_test, y_test)
 print(acc)
 with open("metrics.txt", "w") as outfile:
-    outfile.write("Accuracy: " + str(acc) + "\n")
+    outfile.write("Accuracy is: " + str(acc) + "\n")
 
 # Plot it
 disp = ConfusionMatrixDisplay.from_estimator(
