e70bb502993650e5690ad763b3f0e9123eaa199d
CptCaptain <nilskoch@posteo.de>
2023-06-06 15:23:06 +0200

    Update train.py
    add DVCLive tracking

--- train.py
+++ train.py
@@ -3,9 +3,10 @@ from sklearn.metrics import ConfusionMatrixDisplay
 import matplotlib.pyplot as plt
 import numpy as np
 import yaml
+from dvclive import Live
+
 
 params = yaml.safe_load(open("params.yaml"))["train"]
-
 
 # Read in data
 X_train = np.genfromtxt("data/train_features.csv")
@@ -13,18 +14,21 @@ y_train = np.genfromtxt("data/train_labels.csv")
 X_test = np.genfromtxt("data/test_features.csv")
 y_test = np.genfromtxt("data/test_labels.csv")
 
-# Fit a model
-depth = params.get("depth")
-clf = RandomForestClassifier(max_depth=depth)
-clf.fit(X_train, y_train)
+with Live(save_dvc_exp=True) as live:
+    live.log_params(params)
+    # Fit a model
+    depth = params.get("depth")
+    clf = RandomForestClassifier(max_depth=depth)
+    clf.fit(X_train, y_train)
 
-acc = clf.score(X_test, y_test)
-print(acc)
-with open("metrics.txt", "w") as outfile:
-    outfile.write("Accuracy: " + str(acc) + "\n")
+    acc = clf.score(X_test, y_test)
+    print(acc)
+    with open("metrics.txt", "w") as outfile:
+        outfile.write("Accuracy: " + str(acc) + "\n")
 
-# Plot it
-disp = ConfusionMatrixDisplay.from_estimator(
-    clf, X_test, y_test, normalize="true", cmap=plt.cm.Blues
-)
-plt.savefig("plot.png")
+    # Plot it
+    disp = ConfusionMatrixDisplay.from_estimator(
+        clf, X_test, y_test, normalize="true", cmap=plt.cm.Blues
+    )
+    plt.savefig("plot.png")
+    live.log_image("plot.png", plt)
